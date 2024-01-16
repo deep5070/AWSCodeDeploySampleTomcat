2cafe1f063733694e07a8fa2eba1876e0efcabf8
deep5070 <156658546+deep5070@users.noreply.github.com>
2024-01-16 16:51:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 sudo systemctl stop tomcat
-aws s3 cp s3://tomcatartifact/tomcat/target /tmp
-sudo unzip /tmp/target
+aws s3 cp s3://tomcatartifact/tomcat/target/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /opt/tomcat/webapps/LoginWebApp-1.war
 sudo systemctl start tomcat
