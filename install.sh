00be6333a2972d159a8eb4337dfadadc249c4afb
deep5070 <156658546+deep5070@users.noreply.github.com>
2024-01-29 13:46:46 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo systemctl stop tomcat
 sudo chmod -R 777 /opt/tomcat/webapps
-sudo mv /tmp/LoginWebApp-1.war /opt/tomcat/webapps/LoginWebApp-1.war
+sudo cp /tmp/LoginWebApp-1.war /opt/tomcat/webapps/LoginWebApp-1.war
 sudo systemctl start tomcat
